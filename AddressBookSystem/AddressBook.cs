bdbc8ca80b0b9ea0d0659345e10c2d9abb1deccc
Rahullal <Rahullal432@gmail.com>
2023-05-11 10:30:21 +0530

    Ability to edit existing contact person using their name

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -9,8 +9,8 @@ namespace AddressBookSystem
 {
     class AddressBook
     {
-        public Contact[] contacts;
-        public int count;
+        private Contact[] contacts;
+        private int count;
 
         public AddressBook(int capacity)
         {
@@ -32,6 +32,47 @@ namespace AddressBookSystem
             }
         }
 
+        public void EditContact(string firstName, string lastName)
+        {
+            Contact contact = FindContact(firstName, lastName);
+            if (contact != null)
+            {
+                Console.WriteLine($"Editing contact: {contact.FirstName} {contact.LastName}");
+
+                Console.WriteLine("Enter the updated details:");
+                Console.Write("Address: ");
+                contact.Address = Console.ReadLine();
+                Console.Write("City: ");
+                contact.City = Console.ReadLine();
+                Console.Write("State: ");
+                contact.State = Console.ReadLine();
+                Console.Write("Zip: ");
+                contact.Zip = Console.ReadLine();
+                Console.Write("Phone Number: ");
+                contact.PhoneNumber = Console.ReadLine();
+                Console.Write("Email: ");
+                contact.Email = Console.ReadLine();
+
+                Console.WriteLine("Contact updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Contact with name '{firstName} {lastName}' not found.");
+            }
+        }
+
+        private Contact FindContact(string firstName, string lastName)
+        {
+            foreach (var contact in contacts)
+            {
+                if (contact != null && contact.FirstName == firstName && contact.LastName == lastName)
+                {
+                    return contact;
+                }
+            }
+            return null;
+        }
+
         public void DisplayContacts()
         {
             for (int i = 0; i < count; i++)
@@ -40,4 +81,5 @@ namespace AddressBookSystem
             }
         }
     }
+
 }
